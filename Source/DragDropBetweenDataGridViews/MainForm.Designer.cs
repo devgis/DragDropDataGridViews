8f66bac3c436922842edddb6cc1d1aae81147097
devgis <devgis@163.com>
2022-05-11 14:22:01 +0800

    Backuped
    Backuped

---
+++ Source/DragDropBetweenDataGridViews/MainForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.source_DataGridView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.destination_DataGridView = new System.Windows.Forms.DataGridView();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.source_DataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.destination_DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.source_DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.source_DataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.source_DataGridView.Location = new System.Drawing.Point(12, 28);
+            this.source_DataGridView.Name = "dataGridView1";
+            this.source_DataGridView.RowTemplate.Height = 23;
+            this.source_DataGridView.Size = new System.Drawing.Size(677, 194);
+            this.source_DataGridView.TabIndex = 0;
+            this.source_DataGridView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.sourceDataGridView1_MouseDown);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // dataGridView2
+            // 
+            this.destination_DataGridView.AllowDrop = true;
+            this.destination_DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.destination_DataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.destination_DataGridView.Location = new System.Drawing.Point(12, 254);
+            this.destination_DataGridView.Name = "dataGridView2";
+            this.destination_DataGridView.RowTemplate.Height = 23;
+            this.destination_DataGridView.Size = new System.Drawing.Size(677, 205);
+            this.destination_DataGridView.TabIndex = 1;
+            this.destination_DataGridView.DragEnter += new System.Windows.Forms.DragEventHandler(this.Dest_DataGridView2_DragEnter);
+            this.destination_DataGridView.DragDrop += new System.Windows.Forms.DragEventHandler(this.Dest_DataGridView2_DragDrop);
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.Name = "Column6";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source DataGridView";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Destination DataGridView";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 471);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.destination_DataGridView);
+            this.Controls.Add(this.source_DataGridView);
+            this.Name = "Form1";
+            this.Text = "Drag And Drop between DataGridviews";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.source_DataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.destination_DataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView source_DataGridView;
+        private System.Windows.Forms.DataGridView destination_DataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Source/DragDropBetweenDataGridViews/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                object[] a = {  (3 * i).ToString(), (3 * i + 1).ToString(), (3 * i + 2).ToString() };
+                source_DataGridView.Rows.Add(a);
+            }
+
+            for (int i = 1; i <= 2; i++)
+            {
+                object[] a = { (10 * i).ToString(), (10 * i + 1).ToString(), (10 * i + 2).ToString() };
+                destination_DataGridView.Rows.Add(a);
+            }
+        }
+
+        private void sourceDataGridView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //This is the trick part. With this 'info', you can decide which cell you dropped.
+            DataGridView.HitTestInfo info = source_DataGridView.HitTest(e.X, e.Y);
+
+            List<string> a = new List<string>();
+            a.Add(source_DataGridView.Rows[info.RowIndex].Cells[0].Value.ToString());
+            a.Add(source_DataGridView.Rows[info.RowIndex].Cells[1].Value.ToString());
+            a.Add(source_DataGridView.Rows[info.RowIndex].Cells[2].Value.ToString());
+
+            source_DataGridView.DoDragDrop(a, DragDropEffects.Move);
+            source_DataGridView.Rows.RemoveAt(info.RowIndex);
+        }
+
+        private void Dest_DataGridView2_DragEnter(object sender, DragEventArgs e)
+        {
+            bool bIsList = (e.Data.GetDataPresent(typeof(List<string>)) == true);
+            if (bIsList)
+                e.Effect = DragDropEffects.Move;
+        }
+
+        private void Dest_DataGridView2_DragDrop(object sender, DragEventArgs e)
+        {
+            List<string> robj = (List<string>)e.Data.GetData(typeof(List<string>));
+            object[] b = { robj[0], robj[1], robj[2] };
+            destination_DataGridView.Rows.Add(b);
+        }
+    }
+}
